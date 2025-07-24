b1bc27190e7f66fe00c1762ccef02770de9f6d3d
gleysi <sonia.mireya.miranda@gmail.com>
2025-07-24 17:59:08 -0600

    RN_US-01 folder structure

---
+++ src/screens/HomeParent/Family/Family.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styles from './styles';
+
+const Family = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Search Screen</Text>
+    </View>
+  );
+};
+
+export default Family;
+
